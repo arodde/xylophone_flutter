db8f7aeecfb1ddfbfa384f4bafa8d124b97bf4ff
arodde <roddealain@gmail.comgit config --global user.email roddealain@gmail.comgit config --global user.email roddealain@gmail.com>
2021-06-10 18:14:39 +0200

    son obtenu avec un mp4

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'package:just_audio/just_audio.dart';
+
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  late AudioPlayer player;
+  @override
+  void initState() {
+    super.initState();
+    player = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: SafeArea(
+            child: Center(
+              child: TextButton(
+                onPressed: () async {
+                  print('plop');
+                  await player.setAsset('assets/supercopter_2.mp4');
+                  player.play();
+                },
+                child: Text('Click Me'),
+              ),
+            ),
+          ),
         ),
       ),
     );
