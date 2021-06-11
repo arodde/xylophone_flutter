ef2fc899229236595e66e7c48ace6dfb9dfd36b0
arodde <roddealain@gmail.comgit config --global user.email roddealain@gmail.comgit config --global user.email roddealain@gmail.com>
2021-06-11 11:56:22 +0200

    coloration arc-en-ciel des boutons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:just_audio/just_audio.dart';
@@ -13,6 +14,7 @@ class XylophoneApp extends StatefulWidget {
 
 class _XylophoneAppState extends State<XylophoneApp> {
   late AudioPlayer player;
+
   @override
   void initState() {
     super.initState();
@@ -29,79 +31,94 @@ class _XylophoneAppState extends State<XylophoneApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey.shade600,
         body: SafeArea(
-          child: Column(children: [
-            Center(
-              child: TextButton(
+          child: Column(
+            children: [
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ),
                 onPressed: () async {
                   print('plop');
                   await player.setAsset('assets/note1.mp4');
                   player.play();
                 },
-                child: Text('Click Me'),
+                child: Text(''),
               ),
-            ),
-            Center(
-              child: TextButton(
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
+                ),
                 onPressed: () async {
                   print('plop');
                   await player.setAsset('assets/note2.mp4');
                   player.play();
                 },
-                child: Text('Click Me'),
+                child: Text(''),
               ),
-            ),
-            Center(
-              child: TextButton(
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.yellow.shade400),
+                ),
                 onPressed: () async {
                   print('plop');
                   await player.setAsset('assets/note3.mp4');
                   player.play();
                 },
-                child: Text('Click Me'),
+                child: Text(''),
               ),
-            ),
-            Center(
-              child: TextButton(
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.green.shade300),
+                ),
                 onPressed: () async {
                   print('plop');
                   await player.setAsset('assets/note4.mp4');
+
                   player.play();
                 },
-                child: Text('Click Me'),
+                child: Text(''),
               ),
-            ),
-            Center(
-              child: TextButton(
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.teal.shade200),
+                ),
                 onPressed: () async {
                   print('plop');
                   await player.setAsset('assets/note5.mp4');
                   player.play();
                 },
-                child: Text('Click Me'),
+                child: Text(''),
               ),
-            ),
-            Center(
-              child: TextButton(
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
                 onPressed: () async {
                   print('plop');
                   await player.setAsset('assets/note6.mp4');
                   player.play();
                 },
-                child: Text('Click Me'),
+                child: Text(''),
               ),
-            ),
-            Center(
-              child: TextButton(
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+                ),
                 onPressed: () async {
                   print('plop');
                   await player.setAsset('assets/note7.mp4');
                   player.play();
                 },
-                child: Text('Click Me'),
+                child: Text(''),
               ),
-            ),
-          ]),
+            ],
+            mainAxisAlignment: MainAxisAlignment.start,
+          ),
         ),
       ),
     );
